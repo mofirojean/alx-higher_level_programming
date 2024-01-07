64ccbcf7c4d4e9b3ee0c178fe08ba2c1acbe176b
mofirojean <mofirojean@gmail.com>
2024-01-07 23:06:24 +0100

    added 4-print_square.py and 4-print_square.txt

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """Prints a square
+
+    Args:
+        size (int): length of square
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        [print("#", end='') for j in range(size)]
+        print()
